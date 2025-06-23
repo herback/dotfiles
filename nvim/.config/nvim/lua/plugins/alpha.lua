4d1cee662f0c8bfc2ea96c068f5d1358f64dc07e
Alexander Herback <alexander.herback@gmail.com>
2025-06-23 22:19:28 +0200

    add nvim start page

---
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,33 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.startify")
+
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
+
+		vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#CBA6F7" })
+    dashboard.section.header.opts.hl = "AlphaHeader"
+		alpha.setup(dashboard.opts)
+	end,
+}
