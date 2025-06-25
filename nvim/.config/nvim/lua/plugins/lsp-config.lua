02104af2a2c8615c8d19be3cb99210e80b70fba8
Alexander Herback <alexander.herback@gmail.com>
2025-06-25 23:40:22 +0200

    add completion and snippet support

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,41 +1,52 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		opts = {},
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-        "eslint"
-			},
-		},
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"nvim-lspconfig",
-		config = function()
-			vim.diagnostic.config({
-				virtual_text = true,
-			})
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.java_language_server.setup({})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = { "mason-org/mason.nvim" },
-		opts = {
-			ensure_installed = {
-				"stylua",
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      auto_install = true,
+      ensure_installed = {
+        "ts_ls",
+        "eslint",
+      },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      vim.diagnostic.config({
+        virtual_text = true,
+      })
+      local lspconfig = require("lspconfig")
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.java_language_server.setup({})
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "stylua",
         "prettier",
-			},
-			auto_update = true,
-		},
-	},
+      },
+      auto_update = true,
+    },
+  },
 }
---
+++ nvim/.config/nvim/lua/plugins/test.js
@@ -0,0 +1,7 @@
+const peopleCounter = (people) => {
+  for (let i = 0; i < people; i++) {
+    console.log("Hello Darkness My old friend");
+  }
+};
+
+peopleCounter(5);
